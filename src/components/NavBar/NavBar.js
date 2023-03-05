32a4085667c5b7feaf3f65e3af0287d039dfa3b7
Skullby <nicoyun123@gmail.com>
2023-03-05 14:10:48 -0300

    First Commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './navBar.css';
+import Nav from 'react-bootstrap/Nav';
+
+function NavBar() {
+    return (
+      <nav>
+        <h1>Nicolas Yunes</h1>
+        <ul>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
